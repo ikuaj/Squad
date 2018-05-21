09d1b07fe24c90b157e9a3c84b940430ccbdbae1
james ikua <ikuaj96@gmail.com>
2018-05-21 14:28:31 +0300

    Changed the form structure

---
+++ src/main/java/App.java
@@ -0,0 +1,37 @@
+import java.util.Map;
+import java.util.HashMap;
+import spark.ModelAndView;
+import spark.template.velocity.VelocityTemplateEngine;
+import static spark.Spark.*;
+
+public class App {
+  public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
+
+    get("/", (request, response) -> {
+      Map<String, Object> model = new HashMap<String, Object>();
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/squad", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+  
+        String name = request.queryParams("name");
+        Squad newSquad = new Squad(name);
+        request.session().attribute("squad", newSquad);
+  
+        model.put("template", "templates/success.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+      get("/", (request, response) -> {
+        Map<String, Object> model = new HashMap<String, Object>();
+        model.put("squad", request.session().attribute("squad"));
+        model.put("template", "templates/index.vtl");
+        return new ModelAndView(model, layout);
+      }, new VelocityTemplateEngine());
+
+  }
+}
---
+++ src/main/java/Squad.java
@@ -0,0 +1,11 @@
+public class Squad {
+    private String mName;
+
+    public Squad(String name) {
+        mName = name;
+    }
+
+    public String getName() {
+        return mName;
+    }
+}
